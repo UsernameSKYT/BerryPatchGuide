4fcbd3663aea80e9377e66ec6c7dcc7c5f4496c3
UsernameSKYT <kpjaker@gmail.com>
2026-06-17 23:07:56 +0900

    ci: add unit test step + fix app/build gitignore

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_berry_patchguide_BerryPatchGuideApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.berry.patchguide.BerryPatchGuideApp"
-)
-public final class _com_berry_patchguide_BerryPatchGuideApp {
-}
--- app/build/intermediates/classes/debug/transformDebugClassesWithAsm/dirs/META-INF/proguard/moshi-com.berry.patchguide.data.model.SearchResponse.pro
+++
@@ -1,5 +0,0 @@
--keepnames class com.berry.patchguide.data.model.SearchResponse
--if class com.berry.patchguide.data.model.SearchResponse
--keep class com.berry.patchguide.data.model.SearchResponseJsonAdapter {
-    public <init>(com.squareup.moshi.Moshi);
-}
